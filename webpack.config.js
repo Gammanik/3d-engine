86f10f88dbcdce874be8ef014fd9b27b80d6e202
Bobyshev Nikita <Gammanik@gmail.com>
2018-04-05 22:44:19 +1000

    init webpack & typescript

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist',
+        filename: './bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'src'),
+        compress: false,
+        port: 8080
+    }
+};
+//todo: add/configure sourceMaps
